4b10ec8af59d78684f44ddebc9ae6c258a797a14
Anitra <anitrapaul@gmail.com>
2024-06-10 17:37:27 -0700

    day 1 changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
-let A = dormitory
-// let B = company B
+// let A = 
+// let B = 
 // let C =
 // let D =
 // let E =
@@ -19,19 +19,37 @@ const buttonDEl = document.querySelector('.D')
 const buttonEEl = document.querySelector('.E')
 const buttonFEl = document.querySelector('.F')
 
+const buttonELs = document.querySelector('#message')
+
 
 /*-------------------------------- Functions --------------------------------*/
 function init () {
+console.log('Hi!')
+}
+
+function render() {
+    updateMessage()
+}
+
+function updateMessage() {
+    if ()
+}
+
+function handleClick(event) {
     
 }
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-buttonAEl.addEventListener('click')
-buttonBEl.addEventListener('click')
-buttonCEl.addEventListener('click')
-buttonDEl.addEventListener('click')
-buttonEEl.addEventListener('click')
-buttonFEl.addEventListener('click')
+buttonAEl.addEventListener('click', init)
+buttonBEl.addEventListener('click', init)
+buttonCEl.addEventListener('click', init)
+buttonDEl.addEventListener('click', init)
+buttonEEl.addEventListener('click', init)
+buttonFEl.addEventListener('click', init)
+
+buttonEls.forEach((button) => {
+    button.addEventListener('click', handleClick)
+});
 
 
