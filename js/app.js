869523b6cb953a6ccf8048640a85aeaf87139afb
Anitra <anitrapaul@gmail.com>
2024-06-12 14:35:37 -0700

    update

--- js/app.js
+++ js/app.js
@@ -5,65 +5,40 @@
 // let D;
 // let E;
 // let F;
-function init () {
 
     const game = {
     lives: 3,
     player: 'Player Name',
-    stages: [
-        {
-            title: 'stage 1',
-            message: 'content',
-            graphic: 'image url',
-            playerOptions: [game.stages[1], game.stages[2]]
-        },
-        {
-            title: 'stage 2',
-            message: 'content',
-            graphic: 'image url',
-            playerOptions: [game.stages[2], game.stages[3]]
-        },
-        {
-            title: 'stage 3',
-            message: 'content',
-            graphic: 'image url',
-            playerOptions: [game.stages[3], game.stages[4]]
-        },
-        {
-            title: "stage 4",
-            message: "content",
-            graphic: "image url",
-            playerOptions: [game.stages[4], game.stages[5]]
-          },
-          {
-            title: "stage 5",
-            message: "content",
-            graphic: "image url",
-            playerOptions: [game.stages[5], game.stages[6]]
-          }
-    ]
+    stages: ['stage 1', 'stage 2', 'stage 3', 'stage 4', 'stage 5', 'stage 6'],
 }
-}
+
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let userChoice = ''
 
 
 /*------------------------ Cached Element References ------------------------*/
-const buttonAEl = document.querySelector('#A')
-const buttonBEl = document.querySelector('#B')
-const buttonCEl = document.querySelector('#C')
-const buttonDEl = document.querySelector('#D')
-const buttonEEl = document.querySelector('#E')
-const buttonFEl = document.querySelector('#F')
+// const buttonAEl = document.querySelector('#A')
+// const buttonBEl = document.querySelector('#B')
+// const buttonCEl = document.querySelector('#C')
+// const buttonDEl = document.querySelector('#D')
+// const buttonEEl = document.querySelector('#E')
+// const buttonFEl = document.querySelector('#F')
 
-const buttonELs = document.querySelector('#message')
+const buttonELs = document.querySelector('#buttons')
 
 const messageEl = document.querySelector('#message')
-/*-------------------------------- Functions --------------------------------*/
 
+const storyEl = document.querySelector('#story')
+/*-------------------------------- Functions --------------------------------*/
+function init () {
+    render();
+    buttonELs.forEach((button) => {
+        button.addEventListener('click', handleClick);
+    }
+)
 function render() {
-    updateMessage()
+    
 }
 
 function updateMessage() {
@@ -72,7 +47,7 @@ function updateMessage() {
 
 function handleClick(event) {
 
-}
+}}
 
 
 /*----------------------------- Event Listeners -----------------------------*/
