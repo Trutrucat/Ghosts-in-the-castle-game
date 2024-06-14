4f15f5ae862e33b7d11980d6862ea5b72d8969db
Anitra <anitrapaul@gmail.com>
2024-06-13 18:10:24 -0700

    FINALLY!!!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-/*-------------------------------- Constants --------------------------------*/
 
 const stages = [
     {
@@ -18,8 +17,8 @@ const stages = [
     },
     {
         message: 'You settle in on one of the beds, listening for any sounds. All is quiet. You strain your eyes in the darkness to catch sight of any movement. All is still. Suddenly you hear a sound coming from the floor directly above you. It sounds like footsteps in the dormitory but you know that you are alone in the castle. You decide to climb the stairs and go check out company B.',
-        btnName: ['go upstairs to company B'],
-        gotoOption: [2]
+        btnNames: ['go upstairs to company B'],
+        gotoOptions: [2]
     },
     {
         message: 'You take a deep breath and calm your shaky nerves as you shine your flashlight up into the rafters where you thought you saw something moving. All you can see is the old, worn graffiti from years ago where the boys climbed up to sign their names. You decide that you should further investigate with the equipment you brought along. Do you try to take a photo with your full-spectrum camera? Or do you go the low-tech route and set up a cat ball on the bed to see if any spirits want to touch it and light it up?',
@@ -28,56 +27,50 @@ const stages = [
     },
     {
         message: 'You take your full-spectrum camera from its case and attach the infrared light to it. You carefully aim it at the rafters where you thought you saw movement from the corner of your eye. You stare through the bright view screen and see nothing. You snap a few pictures just in case, but you have a feeling nothing will show up in the photos. Maybe you needed to choose the low tech option. You decide to try the cat ball instead.',
-        btnName: 'try the cat ball',
+        btnNames: ['try the cat ball'],
         gotoOptions: [6]
         
      },
      {
-        message: 'You decide to set up cat ball to see if there is anyone in the room with you. You place one carefully on the bed and call out to whatever spirits may be watching, “I am placing this ball that lights up with just the slightest moment on this bed. If you would like to make your presence known, feel free to show me you are here by lighting it up!”  You turn on your spirit box app on your phone to listen for any voices that may come through. Just as you are turning it on you hear a little voice giggle and say “hello!”. Suddenly the cat ball on the bed next to where you are standing lights up. You smile and say “Thank you for making your presence known!” The ball continues to light up. Congratulations! You are a winner! You just made your very first contact with the spirits of Preston Castle!'
+        message: 'You decide to set up cat ball to see if there is anyone in the room with you. You place one carefully on the bed and call out to whatever spirits may be watching, “I am placing this ball that lights up with just the slightest moment on this bed. If you would like to make your presence known, feel free to show me you are here by lighting it up!”  You turn on your spirit box app on your phone to listen for any voices that may come through. Just as you are turning it on you hear a little voice giggle and say “hello!”. Suddenly the cat ball on the bed next to where you are standing lights up. You smile and say “Thank you for making your presence known!” The ball continues to light up. Congratulations! You are a winner! You just made your very first contact with the spirits of Preston Castle!',
+        btnNames: [],
         
      }
 
 ]
 
-
-/*---------------------------- Variables (state) ----------------------------*/
 let userChoice = 0;
-
-
-/*------------------------ Cached Element References ------------------------*/
-// const buttonAEl = document.querySelector('#A')
-// const buttonBEl = document.querySelector('#B')
-// const buttonCEl = document.querySelector('#C')
-// const buttonDEl = document.querySelector('#D')
-// const buttonEEl = document.querySelector('#E')
-// const buttonFEl = document.querySelector('#F')
 
 const buttonELs = document.querySelectorAll('#buttons button')
 
 const messageEl = document.querySelector('#message')
 
-//const storyEl = document.querySelector('#story')
-/*-------------------------------- Functions --------------------------------*/
 function init () {
     userChoice = 0;
     render();
     buttonELs.forEach((button, index) => {
-        //button.textContent = stages[0].btnNames[index];
         button.addEventListener('click', handleClick);
     }
 )
 function render(buttonText) {
+    console.log('Current stage:', stages[userChoice]);
     messageEl.textContent = stages[userChoice].message;
-    buttonELs.forEach((button, index) => {
-        button.textContent = stages[userChoice].btnNames[index];
-    
-    });
+    if (stages[userChoice].btnNames.length === 1) {
+        buttonELs[0].textContent =stages[userChoice].btnNames[0];
+        buttonELs[1].style.display = 'none'
+    } else {
+        buttonELs.forEach((button, index) => {
+            button.textContent = stages[userChoice].btnNames[index];
+        
+        });
+    }   
 }
-//console log variables inside handleClick before render
 //for conclusion hide buttons game ending message
 
 function handleClick(event) {
     const buttonText = event.target.textContent;
+    console.log('Current user choice:', userChoice);
+    console.log('Current stage:', stages[userChoice]);
     const currentIndex = stages[userChoice].btnNames.indexOf(buttonText);
     if (currentIndex != -1) {
     userChoice = stages[userChoice].gotoOptions[currentIndex];
@@ -86,13 +79,6 @@ function handleClick(event) {
 init();
 
 
-/*----------------------------- Event Listeners -----------------------------*/
-// buttonAEl.addEventListener('click', init)
-// buttonBEl.addEventListener('click', init)
-// buttonCEl.addEventListener('click', init)
-// buttonDEl.addEventListener('click', init)
-// buttonEEl.addEventListener('click', init)
-// buttonFEl.addEventListener('click', init)
 
 
 
